8f3e6359eb74e7d8b7fe81253ab29f5a936116c5
poojapurohit2909 <131505062+Poojapurohit2909@users.noreply.github.com>
2024-02-02 22:28:56 +0530

    30 days of code with csi

---
+++ Day2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Enter your name: ";
+    cin>>name;
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+    cout<<endl<<"Hello, "<<name<<"!"<<endl<<"Your age is "<<age<<".";
+    }
